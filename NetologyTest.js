c97a308b8afef13d9ad0dcea9a6224575b197feb
AnnaAndreeva92 <ann-stesha@yandex.ru>
2023-06-05 18:13:22 +0300

    test

--- NetologyTest.js
+++ NetologyTest.js
@@ -12,6 +12,7 @@ test('validTest', async ({ page }) => {
   await page.locator('[placeholder="Пароль"]').fill(user.password);
   await page.locator('[data-testid="login-submit-btn"]').click();
   await expect(page).toHaveURL('https://netology.ru/profile');
+  await browser.close();
 });
 
 test('noValidTest', async ({ page }) => {
@@ -23,5 +24,7 @@ test('noValidTest', async ({ page }) => {
   await page.locator('[placeholder="Пароль"]').click();
   await page.locator('[placeholder="Пароль"]').fill(user.noValidPassword);
   await page.locator('[data-testid="login-submit-btn"]').click();
-  await expect(page).toHaveURL('https://netology.ru/profile');
+  await expect(page.locator('._-packages-ui-kit-components-v2-Input--error--1QFF1  div'))
+      .toHaveText('Вы ввели неправильно логин или пароль');
+      await browser.close();
 });
